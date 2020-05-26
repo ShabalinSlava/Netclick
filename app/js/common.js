1bd562293f36de9614f0a4beef30fb3a21997b18
Shabalin Slava <svaznoy0309@gmail.com>
2020-05-26 12:35:04 +0800

    added click on menu

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,30 @@
-$(function() {
+document.addEventListener('DOMContentLoaded', function(){
+    // Menu
+    const leftMenu = document.querySelector('.left-menu');
+    const hamburger = document.querySelector('.hamburger');
 
-	// Custom JS
+    // Открытие и закрытие меню
+    hamburger.addEventListener('click', () => {
+      leftMenu.classList.toggle('openMenu');
+      hamburger.classList.toggle('open')
+    });
 
-});
+    // Click вне меню
+    document.addEventListener('click', (event) => {
+      if (!event.target.closest('.left-menu')) {
+        leftMenu.classList.remove('openMenu');
+        hamburger.classList.remove('open')
+      }
+    })
+    // Открытие подменю при клике
+    leftMenu.addEventListener('click', (event) => {
+      const target = event.target;
+      const dropdown = target.closest('.dropdown');
+      if (dropdown) {
+        dropdown.classList.toggle('active')
+        leftMenu.classList.add('openMenu');
+        hamburger.classList.add('open');
+      }
+    });
+
+  });
